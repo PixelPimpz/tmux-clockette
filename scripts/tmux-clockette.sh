fa22f6c54182d40eecd0d2446eed0bd36211566c
lucifer-morningstar <slobeck@gmail.com>
2025-12-27 09:15:19 -0800

    Generic commit

--- scripts/tmux-clockette.sh
+++ scripts/tmux-clockette.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 DEBUG=1
 ((DEBUG == 1)) && tmux display -p "tmux-clockette.sh running..."
+hours=("one" "two" "three" "four" "five" "six" "seven" "eight" "nine" "ten" "eleven" "twelve")
 
 getIcon() {
   eval $(tmux display -p "#{@GET_ICON}") $1
 }
-hours=("one" "two" "three" "four" "five" "six" "seven" "eight" "nine" "ten" "eleven" "twelve")
-h=$( date +%-H )
-time_h=$(( (h%12)-1  ))
-(( time_h == 0 )) && $time_h=12
-hour="${hours[$time_h]}"
+h=$( date +%l )
+hour="${hours[$h]}"
 tmux display -p "time: $hour $(getIcon $hour)"
