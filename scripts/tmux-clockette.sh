1ad3398219f8d5fb658f6b4937519927fa3808aa
lucifer-morningstar <slobeck@gmail.com>
2025-12-27 08:28:52 -0800

    Generic commit

--- scripts/tmux-clockette.sh
+++ scripts/tmux-clockette.sh
@@ -6,7 +6,7 @@ getIcon() {
   eval $(tmux display -p "#{@GET_ICON}") $1
 }
 hours=("one" "two" "three" "four" "five" "six" "seven" "eight" "nine" "ten" "eleven" "twelve")
-time_h=$(( $(( $( date +%-H ) %12 )) -1 ))
+time_h=$( date +%-I )
 (( time_h == 0 )) && $time_h=12
 hour="${hours[$time_h]}"
 tmux display -p "time: $hour"
