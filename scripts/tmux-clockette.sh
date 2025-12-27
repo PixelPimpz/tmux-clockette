ce4b6835dac0d3ae639df3dd8cf3642d4d1e4405
lucifer-morningstar <slobeck@gmail.com>
2025-12-27 06:06:31 -0800

    Generic commit

--- scripts/tmux-clockette.sh
+++ scripts/tmux-clockette.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 DEBUG=1
+getIcon() {
+  eval $(tmux display -p "#{@GET_ICON}") $1
+}
 ((DEBUG == 1)) && tmux display -p "tmux-clockette.sh running..."
-alias getIcon="$(tmux display -p "#{@GET_ICON}")"
 getIcon "alert"
