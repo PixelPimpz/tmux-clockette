e9d5b7d71b14efc0284e490630215a0a5771cb83
lucifer-morningstar <slobeck@gmail.com>
2025-12-27 18:13:23 -0800

    Generic commit

--- scripts/tmux-clockette.sh
+++ scripts/tmux-clockette.sh
@@ -8,20 +8,15 @@ main() {
   local now_ht="${hours[((now_hd-1))]}"
   tmux display -p "Current hour:${now_ht}"
   tmux set -g '@clock' "$(getIcon ${now_ht})"
-  local interval=$(getInterval "$now") 
+  local interval="$(getInterval $now)" 
+  tmux display -p ">> ${interval}"
 }
 
 getInterval() {
   local now="$1"
   local s_to_minute=$((60 - $(awk -F: '{print $3}' <<< $now) )) 
   local m_to_hour=$((60 - $(awk -F: '{print $2}' <<< $now) )) 
-  local s_to_hour=$(( (m_to_hour * 60)+ s_to_minute ))
-  if (( DEBUG == 1 )); then
-    tmux display -p "tmux-clockette.sh is running..."
-    debug "$(tmux display -p "${s_to_minute} seconds to next minute")"
-    debug "$(tmux display -p "${m_to_hour} minutes to next hour")"
-    debug "$(tmux display -p "${s_to_hour} seconds to next hour")"
-  fi
+  local s_to_hour=$(( ( m_to_hour * 60 ) + s_to_minute ))
   echo "${s_to_hour}"
 }
 
