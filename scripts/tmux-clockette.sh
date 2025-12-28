10b1edae58583ac023e8b64662dfc79e28b31d66
lucifer-morningstar <slobeck@gmail.com>
2025-12-27 16:05:51 -0800

    Generic commit

--- scripts/tmux-clockette.sh
+++ scripts/tmux-clockette.sh
@@ -12,7 +12,10 @@ main() {
 
 interval() {
   local now="$(date +%l:%M:%S)"
-  tmux display -p ">> $(echo "$now")" 
+  local delta_s=$((60 - $(awk -F: '{print $3}' <<< $now) )) 
+  local delta_m=$((60 - $(awk -F: '{print $2}' <<< $now) )) 
+
+  tmux display -p "$(printf '%d seconds to the next minute' $delta_s)" 
 }
 
 getIcon() {
