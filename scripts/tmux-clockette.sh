6b55dd7b860847e6f6c204deb3117ab799e20f8c
lucifer-morningstar <slobeck@gmail.com>
2025-12-28 18:43:10 -0800

    Generic commit

--- scripts/tmux-clockette.sh
+++ scripts/tmux-clockette.sh
@@ -1,68 +1,58 @@
 #!/usr/bin/env bash
 DEBUG=1
-GET_ICON=$(tmux display -p "#{@GET_ICON}") 
-## @GET_ICON=/home/lucifer/.config/tmux/plugins/tmux-geticon/scripts/tmux-geticon.sh
-#
-hours=("one" "two" "three" "four" "five" "six" "seven" "eight" "nine" "ten" "eleven" "twelve")
-
+TIMER_PID=
+## get clock icons from nerdfonts
 main() {
-  local now="$(date +%-l:%M:%S)"
-  local now_hd="$(awk -F: '{print $1}' <<< $now)"
-  local now_ht="${hours[$((now_hd-1))]}"
-
-  ## pass the date string to getInterval for parsing
-  local interval="$(getInterval $now)" 
-  (( "$?" != 0 )) && fatal "getInterval failed"
-
-  ## tmux-run getIcon with the text hour
-  local clock=$(tmux run "$GET_ICON $now_ht")
-  debug "tmux-clockette.sh is running..."
-  debug ">> GET_ICON: $GET_ICON"
-  debug ">> clock: $clock"
-  debug ">> Current hour: ${now_ht}"
-  debug ">> ${interval} seconds to $(( now_hd + 1 )):00"
-
-  setClock "$clock"
-  ## check for existing TimerPID and kill the process 
-  # before starting a new one then
-  # start timer to run at top of next hour
-  #Timer "$interval" "main" & TimerPID=$!
+  LOCALTIME="$(date +%I:%M:%S)"
+  CLOCK=$( echo -e "\\U$(printf '%x\n' $((0xF144B + $(date +%I) - 1 )))")
+  INTERVAL=$(getInterval "$LOCALTIME")
+  
+  # report VARS and VALS to stdout
+  debug "scripts/tmux-clockette.sh is running..."
+  debug ">> CLOCK: ${CLOCK}"
+  debug ">> LOCALTIME: ${LOCALTIME}"
+  debug ">> INTERVAL: ${INTERVAL}"
+  
+  # set clock
+  setClock "${CLOCK}"
+  startTimer "$INTERVAL" "cleanup" & TIMER_PID=$!
+  debug ">> TIMER_PID: ${TIMER_PID}"
 }
 
-# set a global tmux option (-g) '@clock' 
-# with the newly acquired clock icon
-setClock() {
-  local clock=$1
-  tmux set -g '@clock' "$clock"
-}
-
-isInt() { [[ $1 =~ ^-?[0-9]+$ ]] }
- 
-getInterval() {
-  local now="$1"
-  local s_to_minute=$((60 - $(awk -F: '{print $3}' <<< $now) )) 
-  local m_to_hour=$((60 - $(awk -F: '{print $2}' <<< $now) )) 
-  local s_to_hour=$(( ( m_to_hour * 60 ) + s_to_minute ))
-  echo "${s_to_hour}"
-}
-
-Timer() {
-  local duration="$1"
-  local action="$2"
-  (( $(isInt $duration) != 0 )) && fatal "duration is not an integer"
-  declare -F "$action" >/dev/null || fatal "$action not declared in this script"
+startTimer() {
+  local SEC="$1"
+  local ACTION="$2"
   (
-    sleep "$duration"
-    debug ">> updating the clock icon at $(date +%l:%M:%S)"
-    "$action"
+    sleep 15
+    debug ">> setting clock at $(date +%T)"
+    "$ACTION"
   ) &
-  debug "timer for $duration running..."
+  debug ">> TIMER for $ACTION started for $SEC seconds"
+}
+
+getInterval() {
+  local T="$1"
+  local s_to_next_m="$(( 60 - $(awk -F: '{printf "%d\n", $3}' <<< $T)))"
+  local m_to_next_h="$(( 60 - $(awk -F: '{printf "%d\n", $2}' <<< $T)))"
+  tmux display -p "$(( (m_to_next_h * 60) + s_to_next_m ))"
 }
 
 debug() {
+  local M="$1"
   if (( DEBUG == 1 )); then
-    local D="$1"
-    tmux display -p "$D"
+    tmux display -p "$M"
   fi
+}
+
+setClock() {
+  local clock="$1"
+  tmux set -g '@clock' "$clock"
+}
+
+cleanup() {
+  local timer_pid="$1"
+  debug ">> Timer complete. Checking for zombie processes..."
+  
+  main
 }
 main
