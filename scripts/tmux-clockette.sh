cb9a35abef87a757a80fa802ddf69f650d1422a8
lucifer-morningstar <slobeck@gmail.com>
2026-01-07 23:21:06 -0800

    Generic commit

--- scripts/tmux-clockette.sh
+++ scripts/tmux-clockette.sh
@@ -40,7 +40,8 @@ setClock() {
   local clock_start=0xF144B
   local hour="$(awk -F: '{printf "%d\n", $1}' <<< $localtime)"
   local clock_now=$(( clock_start + hour - 1 ))
-  local clock="$( printf "\U%x" "$clock_now" )"
+  #local clock="$( printf "\U%x" "$clock_now" )"
+  local clock="$( echo -e "\U${clock_now}" )" 
   tmux set -g '@clock' "$clock"
 }
 
