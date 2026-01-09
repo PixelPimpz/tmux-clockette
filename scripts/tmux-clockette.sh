b71b89519b46afb4a8db6ca099fd21d4d1d15efe
lucifer-morningstar <slobeck@gmail.com>
2026-01-09 13:50:25 -0800

    Generic commit

--- scripts/tmux-clockette.sh
+++ scripts/tmux-clockette.sh
@@ -16,7 +16,7 @@ main() {
   debug ">> clockette START. [CTRL-X] to stop"
 
   while true; do 
-    local localtime=$(date +%-I:%M:%S)
+    local localtime=$(date +%-I:%M:%S:%P)
     local H=$(awk -F: '{print $1}' <<< "$localtime")
     local M=$(awk -F: '{print $2}' <<< "$localtime")
     local S=$(awk -F: '{print $3}' <<< "$localtime")
@@ -40,9 +40,6 @@ setClock() {
   local hex_base="0xF144B"
   local hex_now="$( printf '%X\n' "$(( hex_base + hour - 1 ))")"
   local icon="$( echo -e "\U$hex_now")"
-  debug ">> localtime: $localtime"
-  debug ">> hex_base: $hex_base"
-  debug ">> hex_now: 0x${hex_now}"
   debug ">> icon: $icon"
 }
 
