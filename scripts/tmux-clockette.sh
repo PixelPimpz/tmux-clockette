017e29fd2c471038bd52506d4601d2acbb7ffb1c
lucifer-morningstar <slobeck@gmail.com>
2026-01-23 14:04:47 -0800

    Generic commit

--- scripts/tmux-clockette.sh
+++ scripts/tmux-clockette.sh
@@ -38,7 +38,7 @@ setClock()
   dump ">> icon: ${icon}"
   tmux set -g @clock "$icon"
   tmux set -g @clockette "#[fg=#{@orange_b}]#{@TriangleL}#[bg=#{@bg0},bold]#[reverse]#{@clock} $hour#[blink]:#[noblink]$minute%P #[bg=default]#[noreverse]#{@TriangleRInverse}"
-  tmux set -g @calendar "#[fg=#{@green}]#{@TriangleL}#[bg=#{@bg0},bold]#[reverse]${day:0:1} %m|%e|%Y #[bg=default]#[noreverse]#{@HemiR}"
+  tmux set -g @calendar "#[fg=#{@green}]#{@TriangleL}#[bg=#{@bg0},bold]#[reverse]${day:0:1} %m|%e|%Y#[bg=default]#[noreverse]#{@HemiR}"
 }
 
 main  
